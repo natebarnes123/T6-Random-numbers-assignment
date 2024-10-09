d0631e4c6855d51d9a41da51f6ce2866ac96c333
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-09 08:40:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T6_Random_numbers_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
